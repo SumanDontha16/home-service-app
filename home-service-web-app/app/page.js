6327568cc38a8d6b07d5252f5e659122848934a7
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2024-03-12 10:05:27 +0000

    Project setup with Shadcn UI

---
+++ home-service-web-app/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2> Home Page </h2>
+      <Button>Button</Button>
+    </div>
+  );
+}
